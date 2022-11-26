7ab780a4a0585905be60002d7afb0468ea3920a1
m4cola <mike@mdinicola.com>
2022-11-26 14:27:35 -0500

    spacing

--- crunchyroll_notifier/handlers/api_handler.py
+++ crunchyroll_notifier/handlers/api_handler.py
@@ -63,9 +63,12 @@ def get_recently_added(event, context):
     try:
         crunchyroll_service = _get_crunchyroll_service()
         query_parameters = event['queryStringParameters']
+        
         filter_keys = [ 'sort_by', 'max_results', 'start_value', 'is_dubbed', 'is_subbed', 'time_period_in_days', 'list_id' ]
         filters = handle_filters(query_parameters, filter_keys)
+        
         recently_added = crunchyroll_service.get_recently_added(filters)
+        
         response = {
             'filters': filters,
             'total': len(recently_added),
